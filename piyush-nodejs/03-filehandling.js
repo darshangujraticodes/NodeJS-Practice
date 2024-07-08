0d3f611b1bf6214441803fdd3fe2a205d258cdf8
darshangujraticodes <darshangujrati5@gmail.com>
2024-07-08 15:30:06 +0530

    node js file operation

--- piyush-nodejs/03-filehandling.js
+++ piyush-nodejs/03-filehandling.js
@@ -13,15 +13,15 @@ Synchronous -> file data of sync commands can be stored in variable but not for 
 */
 
 // asynchronous approach
-// fs.writeFile(
-//   "./file1.txt",
-//   "\nHi Darshan here, Full Stack Developer and Adventure Explorer",
-//   (error) => {
-//     if (error) {
-//       console.log("Error in File Operation", error);
-//     }
-//   }
-// );
+fs.writeFile(
+  "./file1.txt",
+  "\nHi Darshan here, Full Stack Developer and Adventure Explorer",
+  (error) => {
+    if (error) {
+      console.log("Error in File Operation", error);
+    }
+  }
+);
 
 fs.appendFile(
   "./file1.txt",
@@ -37,7 +37,7 @@ fs.readFile("./file1.txt", "utf-8", (error, result) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(result);
+    // console.log(result);
   }
 });
 
@@ -47,17 +47,23 @@ fs.copyFile("./file1.txt", "./copyfile1.txt", (error) => {
   }
 });
 
-fs.unlink("./copyfile1.txt", (error) => {
+fs.rename("./copyfile1.txt", "./copy1file1.txt", (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
+
+fs.unlink("./file1a.txt", (error) => {
   if (error) {
     console.log(error);
   }
 });
 
 //  synchronous approach
-// fs.writeFileSync(
-//   "./file2.txt",
-//   "\nHi Darshan here, Full Stack Developer, and Youtube Coding Teacher"
-// );
+fs.writeFileSync(
+  "./file2.txt",
+  "\nHi Darshan here, Full Stack Developer, and Youtube Coding Teacher"
+);
 
 fs.appendFileSync(
   "./file2.txt",
@@ -66,11 +72,11 @@ fs.appendFileSync(
 
 const readOpr2 = fs.readFileSync("./file2.txt", "utf-8");
 
-console.log(readOpr2);
+// console.log(readOpr2);
 
 fs.copyFileSync("./file2.txt", "./copyfile2.txt");
 
-fs.unlinkSync("./copyfile2.txt");
+fs.unlinkSync("./file2a.txt");
 
 /*
 writeFile (Asynchronous) | writeFileSync (Synchronous) => It create new file of specified name and update it with required information
