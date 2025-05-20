ec11322269d3d0b8604767cbdd5da3a29e6b3a6d
darshangujraticodes <darshangujrati5@gmail.com>
2025-05-21 00:27:37 +0530

    mongo db

---
+++ chai-backend/models/ecommerce/customer.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Customer = mongoose.model("Customer", customerSchema);
---
+++ chai-backend/models/ecommerce/product.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Types } from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
